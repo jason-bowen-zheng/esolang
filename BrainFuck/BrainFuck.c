f9759263b74ba1406932980e118696992b544857
jason-bowen-zheng <13239073174@wo.cn>
2020-08-26 08:57:41 +0800

    2020-08-26

--- BrainFuck/BrainFuck.c
+++ BrainFuck/BrainFuck.c
@@ -8,7 +8,7 @@ void run(char *name);
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
-		printf("BrainFuck: expect 2 argumentsi, found %d", argc);
+		printf("BrainFuck: expect 2 args, found %d\n", argc);
 	} else {
 		if (strcmp(argv[1], "--help") == 0) {
 			puts("BrainFuck v1.0\n");
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 # 到底有什么?
 - BrainFuck(就是 fuck your brain 的那个)
+- asm(非常像汇编语言, 但是又不一样)
